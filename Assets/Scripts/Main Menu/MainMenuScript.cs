b7aec1b942f0de4c2a4abcd43103de590a3d427b
Giorgi <giorgi.kuchukhidze@insa-rennes.fr>
2024-11-18 00:32:11 +0100

    Added loading screen

--- Assets/Scripts/Main Menu/MainMenuScript.cs
+++ Assets/Scripts/Main Menu/MainMenuScript.cs
@@ -1,12 +1,43 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject loadingScreenPanel;
+    public Slider loadingSlider;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameScene");
+
+        StartCoroutine(LoadSceneWithProgress("GameScene"));
+    }
+
+    private IEnumerator LoadSceneWithProgress(string sceneName)
+    {
+
+        loadingScreenPanel.SetActive(true);
+
+        loadingSlider.value = 0;
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingSlider.value = progress;
+
+            if (operation.progress >= 0.9f)
+            {
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+
+        loadingScreenPanel.SetActive(false);
     }
 }
